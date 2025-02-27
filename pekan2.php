34398b020271cc281d6fd008794458b16cbe9de9
tiyahh <fathiyahkarim0912@gmail.com>
2025-02-27 14:30:44 +0700

    praktikum02

---
+++ pekan2.php
@@ -0,0 +1,33 @@
+<?php
+//
+/**$i = 1;
+while ($i < 6) {
+    echo $i;
+    $i++;
+}*/
+
+//if statement
+/**if (5>3){
+    echo "Have a nice day!";
+}*/
+
+//if else statement
+/**$t = date("H");
+if ($t < "20"){
+    echo "Have a good day!";
+}else {
+    echo "Have a good night!";
+}*/
+$cars = array("Volvo", "BMW", "Toyota");
+echo $cars[0];
+
+$car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+echo $car["model"];
+
+$cars = array (
+    array("Volvo",22,18),
+    array("BMW",15,13),
+    array("Saab",5,2),
+    array("Land Rover",17,15)
+) ;
+
